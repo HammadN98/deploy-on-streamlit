cd6d1df10ad349181fff7132b9c071a0914b3696
Nimer Hammad <56181068+HammadN98@users.noreply.github.com>
2025-01-19 16:15:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ st.title('Deploy nas Nuvens')
 
 st.info("Este app esta tocando nas nuvens")
 
-url = "https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv"
-dados = pd.read_csv(url)
-dados.head()
+with st.expander('Data'):
+  st.write('**Dados**')
+  dados = pd.read_csv(https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv)
+  dados
